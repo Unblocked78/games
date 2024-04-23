7973850ff3ae6e19d4c3a5a43e13347314b7d711
Unblocked78 <167927955+Unblocked78@users.noreply.github.com>
2024-04-23 16:38:36 -0700

    Update redirectionscript.js

--- redirectionscript.js
+++ redirectionscript.js
@@ -1,1 +1,6 @@
-
+function redirectTo(fromPath, toPath) {
+    if (window.location.pathname === fromPath) {
+        window.location.replace(toPath);
+    }
+}
+redirctTo("/cookieclicker", "/cookieclicker.html")
