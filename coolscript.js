42451352651b35ac4a74350fc129e249e8d62b09
Toegreg <165970604+Toegreg@users.noreply.github.com>
2024-05-03 09:46:02 -0700

    Update coolscript.js

--- coolscript.js
+++ coolscript.js
@@ -1,9 +1,7 @@
-const newTab = window.open('about:blank', '_blank');
-
 const iframe = document.createElement('iframe');
 iframe.src = 'https://unblocked78.github.io/games/';
 iframe.style.width = '100%'; 
 iframe.style.height = '100%';
 iframe.style.border = 'none';
 
-newTab.document.body.appendChild(iframe);
+document.body.appendChild(iframe);
